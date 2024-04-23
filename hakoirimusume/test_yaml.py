7631e4ee523c21718de70a612ffca4e2fc1c0fa3
Keisuke Magara <61485115+kei-mag@users.noreply.github.com>
2024-04-24 02:33:40 +0900

    Restart from 1, initialized spring boot project.

--- hakoirimusume/test_yaml.py
+++
@@ -1,9 +0,0 @@
-import os
-from pprint import pprint
-
-import yaml
-
-os.chdir(os.path.dirname(__file__))
-with open("../instance/hakoirimusume.yml") as file:
-    data = yaml.safe_load(file)
-    pprint(data)
---
+++ src/test/java/net/keimag/hakoirimusume/HakoirimusumeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.keimag.hakoirimusume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HakoirimusumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
