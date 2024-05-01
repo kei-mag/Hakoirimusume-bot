37ad2d28b426d6e07e850fb46f68a86f93943b56
kei-mag <61485115+kei-mag@users.noreply.github.com>
2024-05-02 03:11:05 +0900

    Finish implement function of obtaining BME280 sensor data.

--- src/test/java/net/keimag/hakoirimusume/HakoirimusumeApplicationTests.java
+++ src/test/java/net/keimag/hakoirimusume/HakoirimusumeApplicationTests.java
@@ -3,15 +3,11 @@ package net.keimag.hakoirimusume;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import net.keimag.hakoirimusume.BME280Driver;
-
 @SpringBootTest
 class HakoirimusumeApplicationTests {
 
 	@Test
 	void contextLoads() {
-		var bme280 = new BME280Driver(1, 0x76);
-		bme280.read();
 	}
 
 }
