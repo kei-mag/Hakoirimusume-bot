9615dc4db24d694804301860bf385a20671dedbc
Keisuke Magara <61485115+kei-mag@users.noreply.github.com>
2024-07-03 01:51:31 +0900

    Fix FileNotFoundException when launch from IDE (debug, not bootJar).

--- src/main/java/net/keimag/hakoirimusume/CommonUtil.java
+++ src/main/java/net/keimag/hakoirimusume/CommonUtil.java
@@ -1,16 +1,26 @@
 package net.keimag.hakoirimusume;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 public class CommonUtil {
+    private static final Logger log = LoggerFactory.getLogger(CommonUtil.class);
     private CommonUtil() {}
 
     public static String loadTextResource(String path) throws IOException {
-        InputStream inputStream = new ClassPathResource(path).getInputStream();
-        return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+        try {
+            InputStream inputStream = new ClassPathResource(path).getInputStream();
+            return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+        } catch (FileNotFoundException e) {
+            log.warn("File not found in classpath: {}", path);
+            log.warn("Trying to load from file system...");
+            return java.nio.file.Files.readString(java.nio.file.Paths.get(path));
+        }
     }
 }
